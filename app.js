e7404b7b1a31f77ece70e9c2962abd81a991f4b3
Euan Stirling <euanstirling@gmail.com>
2020-02-20 18:24:53 +0000

    added clear and task filter

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ loadEventListeners();
 function loadEventListeners() {
   //add task event
   form.addEventListener("submit", addTask);
+  //Remove task event
+  taskList.addEventListener("click", removeTask);
 }
+//Clear Task Events
+clearBtn.addEventListener("click", clearTasks);
+
+//Filter Task event
+filter.addEventListener("keyup", filterTasks);
 
 //Add task
 function addTask(e) {
@@ -40,4 +47,37 @@ function addTask(e) {
   taskInput.value = "";
 
   e.preventDefault();
+}
+//Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item"));
+  {
+    if (confirm("Are You Sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Clear Tasks
+function clearTasks() {
+  //taskList.innerHTML = "";
+
+  //Faster method
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+//Filter Taks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
